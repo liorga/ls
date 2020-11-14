869842c742b4c750161daa7da295afaa8cbdb69d
liorga <liorgal31892@gmail.com>
2020-11-14 13:27:59 +0200

    new

--- main.c
+++ main.c
@@ -7,113 +7,121 @@
 #include <grp.h>
 #include <time.h>
 #include <stdlib.h>
+#include "my_ls.h"
+
+
+int main(int argc , char* argv[]) {
+	
+    ls_start(argv[1]);
+
+	return 0;
+}
+
+
+void ls_start(char* file_path){
+    DIR* dp;
+    struct dirent* dir_p;
+    struct stat buff;
+    chdir(file_path);
+    dp = opendir("..");
+    char name[PATH_MAX];
+    while((dir_p = readdir(dp)) != NULL){
+        sprintf(name, "%s/%s", file_path, dir_p->d_name);
+        lstat(dir_p->d_name,&buff);
+        if(S_ISLNK(buff.st_mode)) {
+            printSymbolicLinkStats(&buff,dir_p->d_name);
+        }
+        else {
+            printFileStats(&buff);
+            if(S_ISDIR(buff.st_mode)){
+                printf(GREEN);
+            }
+            if(S_ISREG(buff.st_mode)){
+                printf(RED);
+            }
+            printf("\t%s", dir_p->d_name);
+            printf(DEFAULT_COLOR);
+        }
+        printf("\n");
+    }
+
+    closedir(dp);
+}
+
+void printFileStats(struct stat* fileStats){
+    printf( (S_ISDIR(fileStats->st_mode)) ? "d" : "-");
+    printf( (fileStats->st_mode & S_IRUSR) ? "r" : "-");
+    printf( (fileStats->st_mode & S_IWUSR) ? "w" : "-");
+    printf( (fileStats->st_mode & S_IXUSR) ? "x" : "-");
+    printf( (fileStats->st_mode & S_IRGRP) ? "r" : "-");
+    printf( (fileStats->st_mode & S_IWGRP) ? "w" : "-");
+    printf( (fileStats->st_mode & S_IXGRP) ? "x" : "-");
+    printf( (fileStats->st_mode & S_IROTH) ? "r" : "-");
+    printf( (fileStats->st_mode & S_IWOTH) ? "w" : "-");
+    printf( (fileStats->st_mode & S_IXOTH) ? "x\t" : "-\t");
+
+    printf("%d\t", fileStats->st_nlink);
+
+    struct group *grp;
+    struct passwd *pwd;
+    grp = getgrgid(fileStats->st_gid);
+    printf( "\t%s", grp->gr_name);
+    pwd = getpwuid(fileStats->st_uid);
+    printf(" %s\t\t", pwd->pw_name);
+
+
+    printf(" %zu",fileStats->st_size);
+    if(fileStats->st_size < ONE_HUNDRED){
+        printf("\t");
+    }
+
+    char date[NAME_MAX];
+    strftime(date, NAME_MAX, " %b %d %Y\t", localtime(&(fileStats->st_mtime)));
+    printf("\t%s",date);
+
+}
 
 void printSymbolicLinkStats(struct stat* symLnk,char* dir_p){
-	printf( (S_ISLNK(symLnk->st_mode)) ? "l" : "-");
-	printf( (symLnk->st_mode & S_IRUSR) ? "r" : "-");
-	printf( (symLnk->st_mode & S_IWUSR) ? "w" : "-");
-	printf( (symLnk->st_mode & S_IXUSR) ? "x" : "-");
-	printf( (symLnk->st_mode & S_IRGRP) ? "r" : "-");
-	printf( (symLnk->st_mode & S_IWGRP) ? "w" : "-");
-	printf( (symLnk->st_mode & S_IXGRP) ? "x" : "-");
-	printf( (symLnk->st_mode & S_IROTH) ? "r" : "-");
-	printf( (symLnk->st_mode & S_IWOTH) ? "w" : "-");
-	printf( (symLnk->st_mode & S_IXOTH) ? "x\t" : "-\t");
-	
-	printf("%d\t", symLnk->st_nlink);
+    printf( (S_ISLNK(symLnk->st_mode)) ? "l" : "-");
+    printf( (symLnk->st_mode & S_IRUSR) ? "r" : "-");
+    printf( (symLnk->st_mode & S_IWUSR) ? "w" : "-");
+    printf( (symLnk->st_mode & S_IXUSR) ? "x" : "-");
+    printf( (symLnk->st_mode & S_IRGRP) ? "r" : "-");
+    printf( (symLnk->st_mode & S_IWGRP) ? "w" : "-");
+    printf( (symLnk->st_mode & S_IXGRP) ? "x" : "-");
+    printf( (symLnk->st_mode & S_IROTH) ? "r" : "-");
+    printf( (symLnk->st_mode & S_IWOTH) ? "w" : "-");
+    printf( (symLnk->st_mode & S_IXOTH) ? "x\t" : "-\t");
+
+    printf("%d\t", symLnk->st_nlink);
 
 
-	struct group *grp;
-	struct passwd *pwd;
-	grp = getgrgid(symLnk->st_gid);
-	printf( "\t%s", grp->gr_name);
-	pwd = getpwuid(symLnk->st_uid);
-	printf(" %s\t\t", pwd->pw_name);
+    struct group *grp;
+    struct passwd *pwd;
+    grp = getgrgid(symLnk->st_gid);
+    printf( "\t%s", grp->gr_name);
+    pwd = getpwuid(symLnk->st_uid);
+    printf(" %s\t\t", pwd->pw_name);
 
-	printf(" %zu",symLnk->st_size);
-    if(symLnk->st_size < 100){
+    printf(" %zu",symLnk->st_size);
+    if(symLnk->st_size < ONE_HUNDRED){
         printf("\t");
     }
 
 
-	char date[20];
-	strftime(date, 30, " %b %d %Y\t", localtime(&(symLnk->st_mtime)));
-	printf("\t%s",date);
-	char* buff;
-	ssize_t bufsize ,nbytes;
-	bufsize = symLnk->st_size + 1;
-	if (symLnk->st_size == 0) {
-		bufsize = PATH_MAX;
-	}
-	buff = malloc(bufsize);
-	nbytes = readlink(dir_p,buff,bufsize);
-	printf("\033[0;34m");
-	printf("\t%s -> %.*s", dir_p, (int) nbytes, buff);
-	printf("\033[0m");
-	free(buff);
-}
-
-void printFileStats(struct stat* fileStats){
-	printf( (S_ISDIR(fileStats->st_mode)) ? "d" : "-");
-	printf( (fileStats->st_mode & S_IRUSR) ? "r" : "-");
-	printf( (fileStats->st_mode & S_IWUSR) ? "w" : "-");
-	printf( (fileStats->st_mode & S_IXUSR) ? "x" : "-");
-	printf( (fileStats->st_mode & S_IRGRP) ? "r" : "-");
-	printf( (fileStats->st_mode & S_IWGRP) ? "w" : "-");
-	printf( (fileStats->st_mode & S_IXGRP) ? "x" : "-");
-	printf( (fileStats->st_mode & S_IROTH) ? "r" : "-");
-	printf( (fileStats->st_mode & S_IWOTH) ? "w" : "-");
-	printf( (fileStats->st_mode & S_IXOTH) ? "x\t" : "-\t");
-	
-	printf("%d\t", fileStats->st_nlink);
-
-	struct group *grp;
-	struct passwd *pwd;
-	grp = getgrgid(fileStats->st_gid);
-	printf( "\t%s", grp->gr_name);
-	pwd = getpwuid(fileStats->st_uid);
-	printf(" %s\t\t", pwd->pw_name);
-
-	
-	printf(" %zu",fileStats->st_size);
-	if(fileStats->st_size < 100){
-	    printf("\t");
-	}
-	
-	char date[20];
-	strftime(date, 30, " %b %d %Y\t", localtime(&(fileStats->st_mtime)));
-	printf("\t%s",date);
-
-}
-
-int main(int argc , char* argv[]) {
-	
-	DIR* dp;
-	struct dirent* dir_p;
-	struct stat buff;
-	chdir(argv[1]);
-	dp = opendir("..");
-	char name[PATH_MAX];
-	while((dir_p = readdir(dp)) != NULL){
-		sprintf(name, "%s/%s", argv[1], dir_p->d_name);
-		lstat(dir_p->d_name,&buff);
-		if(S_ISLNK(buff.st_mode)) {
-			printSymbolicLinkStats(&buff,dir_p->d_name);
-		}
-		else {
-			printFileStats(&buff);
-			if(S_ISDIR(buff.st_mode)){
-			    printf("\033[0;32m");
-			}
-			if(S_ISREG(buff.st_mode)){
-                printf("\033[0;31m");
-			}
-			printf("\t%s", dir_p->d_name);
-			printf("\033[0m");
-		}
-		printf("\n");
-	}
-	
-	closedir(dp);
-	return 0;
+    char date[NAME_MAX];
+    strftime(date, NAME_MAX, " %b %d %Y\t", localtime(&(symLnk->st_mtime)));
+    printf("\t%s",date);
+    char* buff;
+    ssize_t bufsize ,nbytes;
+    bufsize = symLnk->st_size + 1;
+    if (symLnk->st_size == 0) {
+        bufsize = PATH_MAX;
+    }
+    buff = malloc(bufsize);
+    nbytes = readlink(dir_p,buff,bufsize);
+    printf(CYAN);
+    printf("\t%s -> %.*s", dir_p, (int) nbytes, buff);
+    printf(DEFAULT_COLOR);
+    free(buff);
 }
--- my_ls.h
+++ my_ls.h
@@ -5,4 +5,16 @@
 #ifndef LS_MY_LS_H
 #define LS_MY_LS_H
 
+#define NAME_MAX 256
+#define ONE_HUNDRED 100
+
+#define GREEN "\033[0;32m"
+#define RED "\033[0;31m"
+#define DEFAULT_COLOR "\033[0m"
+#define CYAN "\033[0;36m"
+
+void printSymbolicLinkStats(struct stat* symLnk,char* dir_p);
+void printFileStats(struct stat* fileStats);
+void ls_start(char* file_path);
+
 #endif //LS_MY_LS_H
